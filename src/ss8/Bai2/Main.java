018b434de5a059f7edf188e6ea01b38e5dc86cd6
hoang0899633314 <ggoswift@gmail.com>
2025-09-13 13:09:10 +0700

    first time

---
+++ src/ss8/Bai2/Main.java
@@ -0,0 +1,16 @@
+package ss8.Bai2;
+
+public class Main {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+        System.out.println(queue.size());
+    }
+}
